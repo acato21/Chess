eb583f9a2c8506758b9af1f1ac169d4937add036
Igor Gabriel <souzaamorimdasilvai@gmail.com>
2024-08-10 18:29:14 -0300

    Board implementation started

---
+++ xadrez-console/Board/Board.cs
@@ -0,0 +1,16 @@
+﻿namespace Board
+{
+    class Board
+    {
+        public int lines { get; set; }
+        public int columns { get; set; }
+        private Part[,] part;
+
+        public Board(int lines, int columns)
+        {
+            this.lines = lines;
+            this.columns = columns;
+            this.part = new Part[lines, columns];
+        }
+    }
+}
