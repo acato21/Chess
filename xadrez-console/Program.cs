b59a9baf8d7f9d2ac3ae03f5dc6d7e91f523ceea
Igor Gabriel <souzaamorimdasilvai@gmail.com>
2024-08-12 19:13:35 -0300

    Creating chess position class

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,22 +7,13 @@ namespace chess
     {
         public static void Main(string[] args)
         {
-            try
-            {
-                Board bd = new Board(8, 8);
-
-                bd.PlacePart(new Tower(Color.Black, bd), new Position(0, 0));
-                bd.PlacePart(new Tower(Color.Black, bd), new Position(1, 9));
-                bd.PlacePart(new King(Color.Black, bd), new Position(0, 2));
-
-                Screen.printBoard(bd);
-
-            }
-            catch (BoardException e) 
-            {
-                Console.WriteLine(e.Message);
-            }
+            ChessPosition pos = new ChessPosition('c', 7);
            
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.ToPosition());
+
+            Console.WriteLine(7 - 'a');
         }
     }
 }
